84762ec674fa7280b893a4e04ee2ab0fba8dc013
PriestSL <sirtokaryev@outlook.com>
2025-01-14 13:25:46 +0200

    restless

--- js/fileStreamGenerator.js
+++ js/fileStreamGenerator.js
@@ -1,31 +1,5 @@
 import { createLoadout } from './textGenerators.js';
 
-const sendConfigToServer = (file) => {
-    let xhr = new XMLHttpRequest();
-    let url = new URL('https://s2-loadout-packer-rest-production.up.railway.app/pack');
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.send(JSON.stringify({file: file}));
-    xhr.onload = function() {
-        if (xhr.status != 200) {
-            console.log(`Error ${xhr.status}: ${xhr.statusText}`);
-        } else {
-            console.log(`Done, got ${xhr.response.length} bytes`);
-            let resp = JSON.parse(xhr.response);
-            let link = document.createElement('a');
-            link.href = 'data:application/octet-stream;base64,' + resp.file;
-            link.download = 's2ArmorStrucGeneratorOutputWEBTEST.pak';
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-            
-        }
-    };
-    xhr.onerror = function() {
-        console.log("Request failed");
-    }
-};
-
 export const generateConfig = async ()=>{
     let cArmorGenerators = "";
     let response = await createLoadout();
@@ -33,9 +7,12 @@ export const generateConfig = async ()=>{
         cArmorGenerators = response;
     }
 
+    let zip = new JSZip();
+    zip.file("Stalker2/Content/GameLite/GameData/ItemGeneratorPrototypes/DynamicItemGenerator.cfg", cArmorGenerators);
+    zip.generateAsync({type:"blob"})
+    .then(function(content) {
+        saveAs(content, "Stalker2Loadout.zip");
+    });
 
-    
-
-    sendConfigToServer(cArmorGenerators);
 }
 
