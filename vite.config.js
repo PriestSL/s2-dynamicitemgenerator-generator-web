da4dc2384d7ca4b6398f10ee459843bf134c8f38
PriestSL <sirtokaryev@outlook.com>
2025-02-06 21:31:57 +0200

    and another one

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ export default {
         minify: true
     },
     server: {
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000,
+        host: true,
     },
     preview: {
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000,
+        host: true,
     }
 }
